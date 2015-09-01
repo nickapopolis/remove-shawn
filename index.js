28011a4c06085f5f2756d20dd448bdfe0b8c11a2
Nick Tierney <ntierney@i-sight.com>
2015-09-01 15:14:26 -0400

    Initial Commit
    Remove Shawn from any collection

---
+++ index.js
@@ -0,0 +1,40 @@
+var _ = require("lodash");
+
+function getRidOfHim(shawnfulArg){
+	if(_.isString(shawnfulArg)){
+		return removeShawnFromString(shawnfulArg);
+	}else if(_.isArray(shawnfulArg)){
+		return removeShawnFromArray(shawnfulArg);
+	}
+	else if (_.isObject(shawnfulArg)){
+		return removeShawnFromObject(shawnfulArg);
+	}else{
+		return shawnfulArg;
+	}
+}
+function removeShawnFromString(shawnfulString){
+	if(shawnfulString == null || shawnfulString.toLowerCase().indexOf("shawn") > -1){
+		return null
+	}else{
+		return shawnfulString;
+	}
+}
+function removeShawnFromObject(shawnfulObject){
+	var shawnlessObject = shawnfulObject;
+	for(var p in shawnlessObject){
+		if(p.toLowerCase().indexOf("shawn") > -1){
+			delete shawnlessObject[p];
+		}else{
+			shawnlessObject[p] = getRidOfHim(shawnlessObject[p]);
+		}
+	}
+	return shawnlessObject;
+}
+function removeShawnFromArray(shawnfulArray){
+	var shawnlessArray = shawnfulArray;
+	for(var i=shawnlessArray.length -1;i >= 0 ; i--){
+		shawnlessArray[i] = getRidOfHim(shawnlessArray[i]);
+	}
+	return _.compact(shawnlessArray);
+}
+module.exports = getRidOfHim;
